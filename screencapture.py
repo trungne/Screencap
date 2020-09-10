3811461acc3c5b067e5a328b3b82da3995221fcd
Trung Nguyen <nguyenquochoangtrung@gmail.com>
2020-09-10 19:07:30 +0700

    Add number at the end of the filename if it already exists

--- screencapture.py
+++ screencapture.py
@@ -6,12 +6,12 @@ from tkinter import *
 def main():
     root = Tk()
     root.title('Screen Capture')
+    selection = Frame(root)
 
     def update(entry, status, curselection):
         # check if user has selected a monitor
         if curselection:  # curseselection return (n,) with n = number of the selection
             monitor = curselection[0]
-            filename = entry.get() + '.png'
         else:
             status['text'] = 'Please select a monitor!'
             return 1
@@ -19,23 +19,25 @@ def main():
         # check if user enters text in the input box
         if entry.get():
             # check if filename already exists
-            if os.path.isfile(filename):
-                status['text'] = filename + ' already exists!'
-            else:
-                screenshot(filename, monitor)
-                status['text'] = filename + ' captured!'
+            filename = entry.get()
+            i = 1
+            while os.path.isfile(filename + '.png'):
+                filename = entry.get() + '-' + str(i)
+                i += 1
+            screenshot(filename, monitor)
+            status['text'] = filename + '.png captured!'
         else:
             status['text'] = 'Please enter something!'
 
     # create widgets
     instruction = Label(root, text='Enter the filename: ')
     inputBox = Entry(root, width=50)
-    screenshot_button = Button(root, text='Screen cap!',
+    screenshot_button = Button(selection, text='Screen cap!',
                                padx=50, pady=20,
                                command=lambda: update(inputBox, statusBar, listBox.curselection()))
-    statusBar = Label(root)
+    statusBar = Label(root, bd=10)
     var1 = IntVar()
-    screenshot_mode = Checkbutton(root, text='Continuous snapping', variable=var1)
+    screenshot_mode = Checkbutton(selection, text='Continuous snapping', variable=var1)
     
     # create options for monitors
     listBox = Listbox(root)
@@ -48,10 +50,15 @@ def main():
     # show widgets
     instruction.grid(row=0, column=0)
     inputBox.grid(row=1, column=0)
-    screenshot_button.grid(row=2, column=0)
-    statusBar.grid(row=3, column=0)
-    listBox.grid(row=4, column=0)
-    screenshot_mode.grid(row=2, column=1)
+
+    # selection frame
+    selection.grid(row=2, column=0)
+    screenshot_button.grid(row=0, column=0)
+    screenshot_mode.grid(row=0, column=1)
+
+    listBox.grid(row=3, column=0)
+    statusBar.grid(row=4, column=0)
+
     # mainloop
     root.mainloop()
 
@@ -64,10 +71,10 @@ def screenshot(filename, monitor_number):
 
         # The screen part to capture
         monitor = {
-            "top": mon["top"],  # + 100,  # 200 px from the top
+            "top": mon["top"],
             "left": mon["left"],
             "width": mon["width"],
-            "height": mon["height"],  # - 150,
+            "height": mon["height"],
             "mon": monitor_number,
         }
         # output = "sct-mon{mon}_{top}x{left}_{width}x{height}.png".format(**monitor)
@@ -76,7 +83,7 @@ def screenshot(filename, monitor_number):
         sct_img = sct.grab(monitor)
 
         # Save to the picture file
-        mss.tools.to_png(sct_img.rgb, sct_img.size, output=filename)
+        mss.tools.to_png(sct_img.rgb, sct_img.size, output=filename + '.png')
         print(filename)
 
 
