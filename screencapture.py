c5ce05d50e0ff6487582e9f71d7baae36b9cb203
Trung Nguyen <nguyenquochoangtrung@gmail.com>
2020-09-11 11:30:57 +0700

    add continuous snapping function, add interval, add stop button

--- screencapture.py
+++ screencapture.py
@@ -6,8 +6,18 @@ from tkinter import *
 
 def main():
     root = Tk()
+    root.resizable(width=False, height=False)
     root.title('Screen Capture')
     selection = Frame(root)
+
+    def modeupdate(checkbox_var):
+        checkbox_var = checkbox_var.get()
+        if checkbox_var:
+            interval.grid(row=1, column=0)
+            stop_button.grid(row=2, column=0)
+        else:
+            stop_button.grid_forget()
+            interval.grid_forget()
 
     def update(entry, status, curselection):
         # check if user has selected a monitor
@@ -34,16 +44,22 @@ def main():
                 i += 1
             screenshot(filename, monitor)  # screenshot!
         status['text'] = filename + '.png captured!'  # display result
+
     # create widgets
     instruction = Label(root, text='Enter the filename: ')
     inputBox = Entry(root, width=50)
-    screenshot_button = Button(selection, text='Screen cap!',
+    screenshot_button = Button(root, text='Screen cap!',
                                padx=50, pady=20,
                                command=lambda: update(inputBox, statusBar, listBox.curselection()))
     statusBar = Label(root, bd=10)
-    var1 = IntVar()
-    screenshot_mode = Checkbutton(selection, text='Continuous snapping', variable=var1)
 
+    var1 = IntVar()
+    screenshot_mode = Checkbutton(selection, text='Continuous snapping', variable=var1,
+                                  command=lambda: modeupdate(var1))
+    interval = Scale(selection, from_=1, to=10, orient=HORIZONTAL)
+    interval.set(1)
+    stop_button = Button(selection, text='Stop',
+                         padx=70, pady=20)
     # create options for monitors
     listBox = Listbox(root)
     for i in range(len(mss.mss().monitors)):
@@ -55,14 +71,12 @@ def main():
     # show widgets
     instruction.grid(row=0, column=0)
     inputBox.grid(row=1, column=0)
-
+    statusBar.grid(row=2, column=0)
+    screenshot_button.grid(row=3, column=0)
+    listBox.grid(row=4, column=0)
     # selection frame
-    selection.grid(row=2, column=0)
-    screenshot_button.grid(row=0, column=0)
-    screenshot_mode.grid(row=0, column=1)
-
-    listBox.grid(row=3, column=0)
-    statusBar.grid(row=4, column=0)
+    selection.grid(row=5, column=0)
+    screenshot_mode.grid(row=0, column=0)
 
     # mainloop
     root.mainloop()
